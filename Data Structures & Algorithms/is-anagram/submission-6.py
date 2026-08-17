a0cd62d810ab2260a069c572f7ff2f75b68fc653
stefelmax <104151391+stefelmax@users.noreply.github.com>
2026-08-17 12:11:52 +0300

    Add: is-anagram - submission-6

---
+++ Data Structures & Algorithms/is-anagram/submission-6.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        hash_s, hash_t = {}, {}
+
+        for char in s:
+            hash_s[char] = hash_s.get(char, 0) + 1
+
+        for char in t:
+            hash_t[char] = 1 + hash_t.get(char, 0)
+
+        return hash_s == hash_t
+        
