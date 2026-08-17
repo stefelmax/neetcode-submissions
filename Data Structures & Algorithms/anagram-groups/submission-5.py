e33801fd02ba277c0bd21ea11801cb53091dde69
stefelmax <104151391+stefelmax@users.noreply.github.com>
2026-08-17 12:54:41 +0300

    Add: anagram-groups - submission-5

---
+++ Data Structures & Algorithms/anagram-groups/submission-5.py
@@ -0,0 +1,23 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+
+        dct = {}
+
+        for word in strs:
+            vector = [0] * 26
+            for char in word:
+                ascii = ord(char) - ord('a')
+                vector[ascii] += 1
+                tuple_vector = tuple(vector)
+            
+            if word == "":
+                tuple_vector = "empty"
+                
+            dct.setdefault(tuple_vector, [])
+            dct[tuple_vector].append(word)
+
+        return list(dct.values())
+
+
+
+        
